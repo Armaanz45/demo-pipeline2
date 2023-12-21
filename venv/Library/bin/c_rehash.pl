42dcf1419ebbaec4a54a1903910121bf49ef2770
Armaanz45 <souliconic7@gmail.com>
2023-12-21 22:52:28 +0530

    Component commit"

--- venv/Library/bin/c_rehash.pl
+++ venv/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "E:/demopipeline2/venv\\Library";
+my $prefix = "e:/demopipeline2/venv\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
